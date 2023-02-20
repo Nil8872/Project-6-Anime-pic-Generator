4f8d14b6fe5a725ca1694380cf6d83ec72d9d7b7
Nilesh <sariyanilesh7265@gmail.com>
2023-02-21 00:34:49 +0530

    version 1.0.0 is Completed.

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const pic = document.querySelector('.pic-container');
 
 const url = "https://api.catboys.com/img"
 btn.addEventListener('click', async ()=>{
-    pic.classList.remove('hidden');
-    imgName.classList.remove('hidden');
-    img.src = "spinner.svg";  
-    const obj = await fetch(url);
-    const mainimg = await obj.json();
-    img.src = await mainimg.url;
-    imgName.innerText = mainimg.artist;
-    console.log(img.src);
+    try{
+
+        pic.classList.remove('hidden');
+        imgName.classList.remove('hidden');
+        imgName.innerText = "Loading...";
+        img.src = "spinner.svg";  
+        const obj = await fetch(url);
+        const mainimg = await obj.json();
+        img.src = await mainimg.url;
+        imgName.innerText = mainimg.artist;
+        console.log(img.src);
+    }catch(e){
+        imgName.innerText = "Some error occure!";
+        
+    }
      
 })
  
