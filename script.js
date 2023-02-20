303837c260614373d4abeb4fb0602d616aea7762
Nilesh <sariyanilesh7265@gmail.com>
2023-02-21 00:28:18 +0530

    In loading time Spinner.svg img is added this image is download from loading.io

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const url = "https://api.catboys.com/img"
 btn.addEventListener('click', async ()=>{
     pic.classList.remove('hidden');
     imgName.classList.remove('hidden');
-    img.src = "https://cdn.dribbble.com/users/93245/screenshots/3231739/media/f8bf3061e45bc9b2c590054f0c5239a3.gif";
+    img.src = "spinner.svg";  
     const obj = await fetch(url);
     const mainimg = await obj.json();
     img.src = await mainimg.url;
